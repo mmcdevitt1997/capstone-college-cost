76a47ac3dc14a04f1ce7873028a11a61744ee9d8
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2019-10-25 14:28:54 -0500

    got basic models

--- collegecost-api/models/collegemodel.py
+++ collegecost-api/models/collegemodel.py
@@ -1,7 +1,9 @@
 from django.db import models
-from django.db.models import F
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .yearmodel import YearModel
 from django.contrib.auth.models import User
-from djmoney.models.fields import MoneyField
+
 
 
 class CollegeModel(models.Model):
@@ -10,11 +12,19 @@ class CollegeModel(models.Model):
     name = models.CharField(max_length=50)
     startyear = models.DateField(null=True, blank=True)
     endyear = models.DateField(null=True, blank=True)
-    debt = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
 
 
 
-
-    class Meta:
-        ordering = (F('user.date_joined').asc(nulls_last=True),)
-
+@receiver(post_save, sender=CollegeModel)
+def create_year( self, sender,  created, **kwargs):
+    # does the math for how many years the user is going to college
+    # by subtracting the end date year by the start date
+    year = self.endyear.strftime('%Y') - self.startyear.strftime('%Y')
+    # This loop should take the number of years and  create the years with the object
+    for numyear in range(year):
+        if created:
+            YearModel.objects.create(
+                name=f"year {numyear+1}",
+                year=numyear+1,
+                college=sender.id
+            )
--- collegecost-api/models/yearmodel.py
+++ collegecost-api/models/yearmodel.py
@@ -5,3 +5,4 @@ class YearModel (models.Model):
     name = models.CharField(max_length=50, blank=True)
     year = models.DateField(null=True, blank=True)
     college = models.ForeignKey(CollegeModel, on_delete=models.DO_NOTHING)
+
